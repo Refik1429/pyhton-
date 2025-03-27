9e5f489462593ae42080d1080a5334509ff12fd2
Refik1429 <hrefikemir@gmail.com>
2025-03-27 15:56:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 meme_dict = {
             "CRINGE": "Garip ya da utandırıcı bir şey",
             "LOL": "Komik bir şeye verilen cevap",
+            "BIZZIKO": "Kendimizinde içinde bulunduğu grup,topluluk",
+            "OK BOOMER": "Karşı tarafı yatıştırmak veya tartışmayı sonlandırmak için kullanılır",
+            "AFK": "Away from keyboard yani bilgisayar başında olmamak anlamına gelir",
+            "STALK": "Birinin sosyal medya hesaplarıına bakma anlamına gelir"
+            "BFF": "
             }
 
 word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
